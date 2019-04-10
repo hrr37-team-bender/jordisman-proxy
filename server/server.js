a3c03a4fedd8ae155bf2a0a2926c5216ac28b2dd
Jordis Man <jordisman@gmail.com>
2019-04-09 19:12:28 -0700

    porxy is working with my product id endpoint

--- server/server.js
+++ server/server.js
@@ -4,12 +4,10 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// app.use(morgan('dev'));
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(morgan());
+// app.use(express.static(path.join(__dirname, '../public')));
 
-// app.get('http://localhost:3003/api/reviews/:productId', (req, res) => {
-
-// })
+app.use('/products/:id', express.static(__dirname + '/../public'));
 
 app.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`);
