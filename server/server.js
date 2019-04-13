ae4016b8921c9d5c4d2998d2b92353b2a5a6ab83
Jordis Man <jordisman@gmail.com>
2019-04-12 23:48:46 -0700

    complete css , banner

--- server/server.js
+++ server/server.js
@@ -1,10 +1,9 @@
 const express = require('express');
-const morgan = require('morgan');
+// const morgan = require('morgan');
 const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(morgan());
 // app.use(express.static(path.join(__dirname, '../public')));
 
 app.use('/products/:id', express.static(__dirname + '/../public'));
